8f0c2e5ff7cd8b3cb34058fc36309b883cf8f856
Carlos Parra <cf.pg@hotmail.com>
2023-03-07 16:47:06 +0100

    Firebase putted

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 import 'view_model/task_view_model.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-   runApp(
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => TaskVM()),
