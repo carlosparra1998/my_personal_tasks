04e7d47361fba967f8a5f0864c6982de54871ef6
Carlos Parra <cf.pg@hotmail.com>
2023-03-11 02:44:41 +0100

    aux pre anim

--- lib/view/home/widgets/list_view.dart
+++ lib/view/home/widgets/list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_personal_tasks/repositories/cache/cache_repository.dart';
 import 'package:provider/provider.dart';
 
 import '../../../model/stream_response.dart';
@@ -19,6 +20,7 @@ class _HomeListView extends State<HomeListView> {
   Widget build(BuildContext context) {
     TaskVM taskViewModel = context.watch<TaskVM>();
     ThemeVM themeViewModel = context.watch<ThemeVM>();
+    final keys = GlobalKey<AnimatedListState>();
 
     return StreamBuilder<Object>(
         stream: taskViewModel.stream,
@@ -30,65 +32,81 @@ class _HomeListView extends State<HomeListView> {
           streamTasks =
               ((snapshot.data) as StreamResponse?) ?? StreamResponse([]);
 
+          Widget buildTask(StreamResponse streamTasks, int index,
+              Animation<double> animation) {
+            TaskVM taskViewModel = context.watch<TaskVM>();
+            ThemeVM themeViewModel = context.watch<ThemeVM>();
+            return FadeTransition(
+              opacity: animation,
+              child: Card(
+                color: themeViewModel.cardColor,
+                child: ListTile(
+                  onTap: () {
+                    modifyTaskDialog(
+                      context,
+                      streamTasks!.response[index].title,
+                      streamTasks.response[index].description,
+                      streamTasks.response[index].priorityLevel,
+                      streamTasks.response[index].id,
+                    );
+                  },
+                  contentPadding: const EdgeInsets.all(0),
+                  leading: Checkbox(
+                    side: BorderSide(
+                      color: (streamTasks!.response[index].priorityLevel == 1)
+                          ? Colors.red
+                          : (streamTasks.response[index].priorityLevel == 2
+                              ? Colors.orange
+                              : Colors.blue), //your desire colour here
+                      width: 2.5,
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    onChanged: (bool? value) {
+                      keys.currentState!.removeItem(
+                          index,
+                          (context, animation) =>
+                              buildTask(streamTasks!, index, animation),
+                          duration: Duration(seconds: 1));
+                      taskViewModel.removeTaskInList(
+                          index, streamTasks!.response[index].id);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        duration: const Duration(milliseconds: 2300),
+                        content: const Text(s.taskComplete),
+                        action: SnackBarAction(
+                          label: s.undoAction,
+                          onPressed: () {
+                            taskViewModel.undoRemoveTaskInList();
+
+                            setState(() {});
+                          },
+                        ),
+                      ));
+                    },
+                    value: false,
+                  ),
+                  title: Text(
+                    streamTasks.response[index].title,
+                    style: TextStyle(color: themeViewModel.fontColor),
+                  ),
+                  subtitle: Text(
+                    streamTasks.response[index].description,
+                    style: TextStyle(color: themeViewModel.subtitleColor),
+                  ),
+                ),
+              ),
+            );
+          }
+
           return Container(
             color: themeViewModel.themeColor,
             child: Center(
-                child: ListView.builder(
+                child: AnimatedList(
+              key: keys,
               padding: const EdgeInsets.all(8),
-              itemCount: streamTasks.response.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  color: themeViewModel.cardColor,
-                  child: ListTile(
-                    onTap: () {
-                      modifyTaskDialog(
-                        context,
-                        streamTasks!.response[index].title,
-                        streamTasks.response[index].description,
-                        streamTasks.response[index].priorityLevel,
-                        streamTasks.response[index].id,
-                      );
-                    },
-                    contentPadding: const EdgeInsets.all(0),
-                    leading: Checkbox(
-                      side: BorderSide(
-                        color: (streamTasks!.response[index].priorityLevel == 1)
-                            ? Colors.red
-                            : (streamTasks.response[index].priorityLevel == 2
-                                ? Colors.orange
-                                : Colors.blue), //your desire colour here
-                        width: 2.5,
-                      ),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      onChanged: (bool? value) {
-                        taskViewModel.removeTaskInList(
-                            index, streamTasks!.response[index].id);
-
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          duration: const Duration(milliseconds: 2300),
-                          content: const Text(s.taskComplete),
-                          action: SnackBarAction(
-                            label: s.undoAction,
-                            onPressed: () {
-                              taskViewModel.undoRemoveTaskInList();
-                              setState(() {});
-                            },
-                          ),
-                        ));
-                      },
-                      value: false,
-                    ),
-                    title: Text(
-                      streamTasks.response[index].title,
-                      style: TextStyle(color: themeViewModel.fontColor),
-                    ),
-                    subtitle: Text(
-                      streamTasks.response[index].description,
-                      style: TextStyle(color: themeViewModel.subtitleColor),
-                    ),
-                  ),
-                );
+              initialItemCount: streamTasks.response.length,
+              itemBuilder: (context, index, animation) {
+                return buildTask(streamTasks!, index, animation);
               },
             )),
           );
